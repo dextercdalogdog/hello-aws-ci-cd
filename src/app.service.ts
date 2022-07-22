98ce9f5921fdf4b5c9c64935254443fecb1dba4b
Dexter Dalogdog <dx.dalogdog@gmail.com>
2022-07-22 15:37:02 +0800

    test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `Welcome to CI/CD test2`;
+    return `Welcome to CI/CD expertiments`;
   }
 }
